a85d70175d349d71701e2c81261f5b65c03a8be6
Emon <ishrakuzzamanemon201@gmail.com>
2025-02-25 20:54:25 +0100

    Taking Array Input

---
+++ FunctionInArray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+// Function prototype for taking array input
+void TakingInputElement(double Array[], int Size);
+
+int main()
+{
+    int size; // Variable to store the size of the array
+    std::cout << "Enter the size of the array: ";
+    std::cin >> size; // User input for array size
+
+    double* array = new double[size]; // Dynamic memory allocation for the array
+    std::cout << "\nEnter the elements of the array" << std::endl;
+    std::cout << "---------------------------------------------" << std::endl;
+
+    TakingInputElement(array, size); // Function call to take array input
+
+    delete[] array; // Deallocate dynamically allocated memory
+    return 0;
+}
+
+// Function to take user input for array elements
+void TakingInputElement(double Array[], int Size)
+{
+    for (int i = 0; i < Size; i++)
+    {
+        std::cout << "Element " << i + 1 << ": ";
+        std::cin >> Array[i];
+    }
+}
