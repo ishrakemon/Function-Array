01ab62a280c23c6d6323df1a4030c7f1153451d4
Emon <ishrakuzzamanemon201@gmail.com>
2025-02-25 20:58:04 +0100

    Printing Array Output

--- FunctionInArray.cpp
+++ FunctionInArray.cpp
@@ -2,6 +2,8 @@
 
 // Function prototype for taking array input
 void TakingInputElement(double Array[], int Size);
+// Function prototype for showing array output
+void ShowingOutputElement(double Array[], int Size);
 
 int main()
 {
@@ -15,6 +17,10 @@ int main()
 
     TakingInputElement(array, size); // Function call to take array input
 
+    std::cout << "\nArray: ";
+    ShowingOutputElement(array, size); // Function call to show array output
+    std::cout << std::endl;
+
     delete[] array; // Deallocate dynamically allocated memory
     return 0;
 }
@@ -26,5 +32,14 @@ void TakingInputElement(double Array[], int Size)
     {
         std::cout << "Element " << i + 1 << ": ";
         std::cin >> Array[i];
+    }
+}
+
+// Function to show user-given input for array elements
+void ShowingOutputElement(double Array[], int Size)
+{
+    for (int i = 0; i < Size; i++)
+    {
+        std::cout << Array[i] << " ";
     }
 }
